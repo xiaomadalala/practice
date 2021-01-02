af26f1c1b7d54f3b9b3b42161b3b4c5d14ebc054
mafuquan <mafuquan@everelegance.com.cn>
2021-01-02 20:07:11 +0800

    add

--- practice/src/main/java/RunTest.java
+++ practice/src/main/java/RunTest.java
@@ -10,7 +10,7 @@ public class RunTest {
     public static void main(String[] args) throws InterruptedException {
         Runer runer1 = new Runer("1");
         Runer runer2 = new Runer("2");
-
+        /1
 
         Thread thread1 = new Thread(runer1);
         Thread thread2 = new Thread(runer2);
